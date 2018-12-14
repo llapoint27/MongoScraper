da2e891f09e4eb0465100042a9a86041838dd8a2
Marco Polo <laurenlapoint@Laurens-MacBook-Pro.local>
2018-12-14 16:54:55 -0500

    adding server code to start up mongod/troubleshoot

--- server.js
+++ server.js
@@ -1,47 +1,59 @@
-// Dependencies
-const express = require("express");
-const bodyParser = require("body-parser"); //JSON responses
-const mongoose = require("mongoose"); //Mongo object modelling 
-const request = require("request"); //Makes http calls
-const cheerio = require("cheerio"); //Scraper
+// // Dependencies
+// const express = require("express");
+// const bodyParser = require("body-parser"); //JSON responses
+// const mongoose = require("mongoose"); //Mongo object modelling 
+// const request = require("request"); //Makes http calls
+// const cheerio = require("cheerio"); //Scraper
+
+// // Require all models
+// const db = require("./models");
+
+// // Port configuration for local and Heroku
+// const PORT = process.env.PORT || process.argv[2] || 3000;
+
+// // Initialize Express
+// const app = express();
+
+// // Use body-parser for handling form submissions
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// // Handlebars
+// const exphbs = require("express-handlebars");
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+
+// // Use express.static to serve the public folder as a static d
+/////////////////////////////////////////////////////////////////////////////
+
+
+var express = require("express");
+// var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
 
 // Require all models
-const db = require("./models");
+var db = require("./models");
 
-// Port configuration for local and Heroku
-const PORT = process.env.PORT || process.argv[2] || 3000;
+var PORT = 3010;
 
 // Initialize Express
-const app = express();
+var app = express();
 
-// Use body-parser for handling form submissions
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Handlebars
-const exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-// Use express.static to serve the public folder as a static directory
+// Configure middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
 app.use(express.static("public"));
-// Controllers
-const router = require("./controllers/routes.js");
-app.use(router);
-// Connect to the MongoDB
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoArticles";
 
-// Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
-// Start/connecting to the server
+// Start the server
 app.listen(PORT, function () {
-    console.log(`This app is successfully running on port: ${PORT}`);
+   console.log("App running on port " + PORT + "!");
 });
-
-
-
-
-
-
